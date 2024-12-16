0fa03c162796299b1ef84ce1560b7f766151da90
sumit <sumit.kumar@resourcestack.com>
2024-12-16 21:37:33 +0530

    integrated test suite import functionality

--- app/controllers/test_suites_controller.rb
+++ app/controllers/test_suites_controller.rb
@@ -95,6 +95,27 @@ class TestSuitesController < ApplicationController
     end
   end
 
+  def import
+    if request.post?
+      @test_suite = TestSuite.new(test_suite_import_params)
+      errors = []
+      errors << 'Import file must be present.' unless test_suite_import_params[:import_suite_cases].present?
+
+      @test_suite.user = Current.user
+      @test_suite.environment = @environment
+
+      if @test_suite.save and errors.blank?
+        flash[:success] = "Test suite '#{@test_suite.name}' & #{@test_suite.test_cases.count} test case(s) have been imported successfully."
+        redirect_to environment_test_suites_path(@environment)
+      else
+        errors << @test_suite.errors.full_messages
+        flash[:errors] = errors.flatten
+      end
+    else
+      @test_suite = TestSuite.new
+    end
+  end
+
   private
 
   def get_environment
@@ -109,6 +130,9 @@ class TestSuitesController < ApplicationController
     params.require(:test_suite).permit(:name, :description, :short_description, :test_plan_id, :is_automated, :jira_id, :base_suite_id, :post_suite_id, :status, :is_stale)
   end
 
+  def test_suite_import_params
+    params.require(:test_suite).permit(:name, :short_description, :is_automated, :import_suite_cases, :dependency)
+  end
   def assign_manual_test_suite_params
     params.require(:assign_manual_test_suite).permit(:test_suite_id, :browser, assign_to: [])
   end
